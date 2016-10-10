3b66618f8f38b7e0eb27ac80a800eb5a6a9cb410
Enrico Gueli <enrico.gueli@gmail.com>
2016-10-10 08:23:24 +0200

    * explicit rows/columns variables * removed keyboard/mouse input

--- src/com/e_gueli/Main.java
+++ src/com/e_gueli/Main.java
@@ -6,7 +6,10 @@ import processing.core.PApplet;
 public class Main extends PApplet {
 
     // Size of cells
-    private int cellSize = 5;
+    int cellSize = 5;
+
+    int rows = 40;
+    int columns = 40;
 
     // How likely for a cell to be alive at start (in percentage)
     float probabilityOfAliveAtStart = 15;
@@ -34,16 +37,16 @@ public class Main extends PApplet {
 
     public void setup() {
         // Instantiate arrays
-        cells = new int[width/cellSize][height/cellSize];
-        cellsBuffer = new int[width/cellSize][height/cellSize];
+        cells = new int[columns][rows];
+        cellsBuffer = new int[columns][rows];
 
         // This stroke will draw the background grid
         stroke(48);
 
 
         // Initialization of cells
-        for (int x=0; x<width/cellSize; x++) {
-            for (int y=0; y<height/cellSize; y++) {
+        for (int x=0; x<columns; x++) {
+            for (int y=0; y<rows; y++) {
                 float state = random (100);
                 if (state > probabilityOfAliveAtStart) {
                     state = 0;
@@ -60,8 +63,8 @@ public class Main extends PApplet {
     public void draw() {
 
         //Draw grid
-        for (int x=0; x<width/cellSize; x++) {
-            for (int y=0; y<height/cellSize; y++) {
+        for (int x=0; x<columns; x++) {
+            for (int y=0; y<rows; y++) {
                 if (cells[x][y]==1) {
                     fill(alive); // If alive
                 }
@@ -78,53 +81,26 @@ public class Main extends PApplet {
                 lastRecordedTime = millis();
             }
         }
-
-        // Create  new cells manually on pause
-        if (pause && mousePressed) {
-            // Map and avoid out of bound errors
-            int xCellOver = (int)Math.floor(map(mouseX, 0, width, 0, width/cellSize));
-            xCellOver = constrain(xCellOver, 0, width/cellSize-1);
-            int yCellOver = (int)Math.floor(map(mouseY, 0, height, 0, height/cellSize));
-            yCellOver = constrain(yCellOver, 0, height/cellSize-1);
-
-            // Check against cells in buffer
-            if (cellsBuffer[xCellOver][yCellOver]==1) { // Cell is alive
-                cells[xCellOver][yCellOver]=0; // Kill
-                fill(dead); // Fill with kill color
-            }
-            else { // Cell is dead
-                cells[xCellOver][yCellOver]=1; // Make alive
-                fill(alive); // Fill alive color
-            }
-        }
-        else if (pause && !mousePressed) { // And then save to buffer once mouse goes up
-            // Save cells to buffer (so we opeate with one array keeping the other intact)
-            for (int x=0; x<width/cellSize; x++) {
-                for (int y=0; y<height/cellSize; y++) {
-                    cellsBuffer[x][y] = cells[x][y];
-                }
-            }
-        }
     }
 
 
 
     void iteration() { // When the clock ticks
         // Save cells to buffer (so we opeate with one array keeping the other intact)
-        for (int x=0; x<width/cellSize; x++) {
-            for (int y=0; y<height/cellSize; y++) {
+        for (int x=0; x<columns; x++) {
+            for (int y=0; y<rows; y++) {
                 cellsBuffer[x][y] = cells[x][y];
             }
         }
 
         // Visit each cell:
-        for (int x=0; x<width/cellSize; x++) {
-            for (int y=0; y<height/cellSize; y++) {
+        for (int x=0; x<columns; x++) {
+            for (int y=0; y<rows; y++) {
                 // And visit all the neighbours of each cell
                 int neighbours = 0; // We'll count the neighbours
                 for (int xx=x-1; xx<=x+1;xx++) {
                     for (int yy=y-1; yy<=y+1;yy++) {
-                        if (((xx>=0)&&(xx<width/cellSize))&&((yy>=0)&&(yy<height/cellSize))) { // Make sure you are not out of bounds
+                        if (((xx>=0)&&(xx<columns))&&((yy>=0)&&(yy<rows))) { // Make sure you are not out of bounds
                             if (!((xx==x)&&(yy==y))) { // Make sure to to check against self
                                 if (cellsBuffer[xx][yy]==1){
                                     neighbours ++; // Check alive neighbours and count them
@@ -147,34 +123,6 @@ public class Main extends PApplet {
             } // End of y loop
         } // End of x loop
     } // End of function
-
-    public void keyPressed() {
-        if (key=='r' || key == 'R') {
-            // Restart: reinitialization of cells
-            for (int x=0; x<width/cellSize; x++) {
-                for (int y=0; y<height/cellSize; y++) {
-                    float state = random (100);
-                    if (state > probabilityOfAliveAtStart) {
-                        state = 0;
-                    }
-                    else {
-                        state = 1;
-                    }
-                    cells[x][y] = (int)Math.floor(state); // Save state of each cell
-                }
-            }
-        }
-        if (key==' ') { // On/off of pause
-            pause = !pause;
-        }
-        if (key=='c' || key == 'C') { // Clear all
-            for (int x=0; x<width/cellSize; x++) {
-                for (int y=0; y<height/cellSize; y++) {
-                    cells[x][y] = 0; // Save all to zero
-                }
-            }
-        }
-    }
 
     public static void main(String... args){
         PApplet.main(Main.class.getName());
