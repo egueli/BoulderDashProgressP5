800c5b585b47e0301bc7781729bde00ab57ab498
Enrico Gueli <enrico.gueli@gmail.com>
2016-10-13 08:50:40 +0200

    initialize cells arrays

--- src/com/e_gueli/KotlinProcessing.kt
+++ src/com/e_gueli/KotlinProcessing.kt
@@ -3,10 +3,23 @@ package com.e_gueli
 import processing.core.PApplet
 import kotlin.reflect.jvm.jvmName
 
+const val columns = 8
+const val rows = 8
+
 class KotlinP5 : PApplet() {
+
+    var emptyColumn = IntArray(rows)
+    var cells = Array(columns, {emptyColumn})
+    var cellsBuffer = Array(columns, {emptyColumn})
+
     override fun settings() {
         size(200, 200)
     }
+
+    override fun setup() {
+        stroke(48)
+    }
+
     override fun draw() {
         background(0)
         ellipse(mouseX.toFloat(), mouseY.toFloat(), 20f, 20f)
