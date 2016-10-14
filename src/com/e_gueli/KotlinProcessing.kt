d5b510d4b0acd2897059e47d532a2d19ae446921
Enrico Gueli <enrico.gueli@gmail.com>
2016-10-14 17:01:10 +0200

    refactor

--- src/com/e_gueli/KotlinProcessing.kt
+++ src/com/e_gueli/KotlinProcessing.kt
@@ -64,32 +64,36 @@ class KotlinP5 : PApplet() {
         // Visit each cell:
         for (x in 0..columns - 1) {
             for (y in 0..rows - 1) {
-                // And visit all the neighbours of each cell
-                var neighbours = 0 // We'll count the neighbours
-                for (xx in x - 1..x + 1) {
-                    for (yy in y - 1..y + 1) {
-                        if (xx >= 0 && xx < columns && yy >= 0 && yy < rows) { // Make sure you are not out of bounds
-                            if (!(xx == x && yy == y)) { // Make sure to to check against self
-                                if (cellsBuffer[xx][yy] == 1) {
-                                    neighbours++ // Check alive neighbours and count them
-                                }
-                            } // End of if
-                        } // End of if
-                    } // End of yy loop
-                } //End of xx loop
-                // We've checked the neigbours: apply rules!
-                if (cellsBuffer[x][y] == 1) { // The cell is alive: kill it if necessary
-                    if (neighbours < 2 || neighbours > 3) {
-                        cells[x][y] = 0 // Die unless it has 2 or 3 neighbours
-                    }
-                } else { // The cell is dead: make it live if necessary
-                    if (neighbours == 3) {
-                        cells[x][y] = 1 // Only if it has 3 neighbours
-                    }
-                } // End of if
-            } // End of y loop
-        } // End of x loop
+                processCell(x, y)
+            }
+        }
 
+    }
+
+    private fun processCell(x: Int, y: Int) {
+        // And visit all the neighbours of each cell
+        var neighbours = 0 // We'll count the neighbours
+        for (xx in x - 1..x + 1) {
+            for (yy in y - 1..y + 1) {
+                if (xx >= 0 && xx < columns && yy >= 0 && yy < rows) { // Make sure you are not out of bounds
+                    if (xx == x && yy == y) continue
+                    if (cellsBuffer[xx][yy] == 1) {
+                        neighbours++ // Check alive neighbours and count them
+                    } // End of if
+                } // End of if
+            } // End of yy loop
+        } //End of xx loop
+        
+        // We've checked the neigbours: apply rules!
+        if (cellsBuffer[x][y] == 1) { // The cell is alive: kill it if necessary
+            if (neighbours < 2 || neighbours > 3) {
+                cells[x][y] = 0 // Die unless it has 2 or 3 neighbours
+            }
+        } else { // The cell is dead: make it live if necessary
+            if (neighbours == 3) {
+                cells[x][y] = 1 // Only if it has 3 neighbours
+            }
+        } // End of if
     }
 
     fun runMain() {
