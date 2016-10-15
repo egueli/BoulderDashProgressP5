9b2b8f641b68732dd04f6ad36985610aa810058c
Enrico Gueli <enrico.gueli@gmail.com>
2016-10-15 09:53:46 +0200

    some more RxKotlin play

--- src/com/e_gueli/RxKotlinExample.kt
+++ src/com/e_gueli/RxKotlinExample.kt
@@ -1,7 +1,10 @@
 package com.e_gueli
 
+import rx.Observable
+import rx.Subscriber
 import rx.lang.kotlin.fold
 import rx.lang.kotlin.observable
+import java.util.*
 
 fun main(args: Array<String>) {
     observable<String> { subscriber ->
@@ -16,4 +19,11 @@ fun main(args: Array<String>) {
             .fold (StringBuilder()) { sb, e -> sb.append(e) }
             .map { it.toString() }
             .subscribe { println(it) }
+
+    Observable.create(Observable.OnSubscribe<Int> { subscriber ->
+        for (i in 1..5)
+            subscriber.onNext(i)
+
+        subscriber.onCompleted()
+    }).subscribe( ::println )
 }
